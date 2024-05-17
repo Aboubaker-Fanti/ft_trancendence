0d58b4a234b27e0e99b03c238935145c688406dd
aboubaker fanti <fantiaboubaker@gmail.com>
2024-05-17 16:06:51 +0100

    first commit

---
+++ js/home.js
@@ -0,0 +1,18 @@
+const body = document.querySelector("body"),
+      sidebar = body.querySelector(".sidebar"),
+      toggle = body.querySelector(".toggle"),
+      searchBtn = body.querySelector(".search-box .icon"),
+      modeSwitch = body.querySelector(".toggle-switch"),
+      modeText = body.querySelector(".mode-text");
+
+      toggle.addEventListener("click", () => {
+        sidebar.classList.toggle("close");
+      });
+      
+      searchBtn.addEventListener("click", () => {
+        sidebar.classList.toggle("close");
+      });
+      
+      modeSwitch.addEventListener("click", () => {
+        body.classList.toggle("dark");
+      });
