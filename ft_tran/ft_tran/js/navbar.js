fa3c2a6d6791778122dec2ba8a2af0c6cddb96db
aboubaker fanti <fantiaboubaker@gmail.com>
2024-05-20 12:00:26 +0100

    with another copy of ft_trance with js and some merge

---
+++ ft_tran/ft_tran/js/navbar.js
@@ -0,0 +1,68 @@
+// here are dark mode jand navbar js code
+
+const body = document.querySelector("body"),
+      toggle = body.querySelector(".toggle"),
+      sidebar = body.querySelector(".sidebar"),
+      searchBtn = body.querySelector(".search-box .icon"),
+      modeSwitch = body.querySelector(".toggle-switch");
+      
+      toggle.addEventListener("click", () => {
+        sidebar.classList.toggle("close");
+      });
+      
+      searchBtn.addEventListener("click", () => {
+        sidebar.classList.toggle("close");
+      });
+      
+      modeSwitch.addEventListener("click", () => {
+        body.classList.toggle("dark");
+      });
+
+
+// here will be single page aplication for navpages
+
+const homepage = document.getElementById('homepage'),
+      gamepage = document.getElementById('gamepage'),
+      profilepage = document.getElementById('profilepage'),
+      inboxpage = document.getElementById('inboxpage'),
+      sittingpage = document.getElementById('sittingpage'),
+      logout = document.getElementById('logout');
+
+    const routing = (event) => {
+        alert("hello we fjksjdfkj");
+        event = event || window.event;
+        event.preventDefault();
+        window.history.pushState({}, "", event.target.href);
+        handlelocation();
+      };
+homepage.addEventListener('click', routing);
+gamepage.addEventListener('click', routing);
+profilepage.addEventListener('click', routing);
+inboxpage.addEventListener('click', routing);
+sittingpage.addEventListener('click', routing);
+
+
+
+
+const routes = {
+  404: "i need a 404 error page",
+  "/": "../html/index.html",
+  "/profile": "../html/profile.html",
+  "/sitting": "../html/sitting.html",
+  "/inpox": "../html/index.html",
+  "/game": "../html/index.html",
+};
+
+const handlelocation = async () => {
+  const path = window.location.pathname;
+  alert(path);
+  const routing = routes[path] || routes[404];
+  const html = await fetch(routing).then((data) => data.text());
+  alert(html);
+  document.querySelector(".container #main-page").innerHTML = html;
+};
+
+window.onpopstate = handlelocation;
+window.routing = routing;
+
+// handlelocation();
